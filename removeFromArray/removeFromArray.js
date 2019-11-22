f7e7b725e725a390095bdf38b85c14c9fdbdbdd4
Angel <cheangel@gmail.com>
2019-11-22 11:17:26 -0500

    Finish excercises

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function ([...arr],...args) {
+    let search = Array.from(args)
+    for (let i = 0; i < search.length; i++) {
+        if (arr.includes(search[i])) {
+            arr.splice(arr.indexOf(search[i]), 1)
+        }
+    }
+    return arr
 }
 
 module.exports = removeFromArray
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        let output = '';
+        for (let i = 0; i < num; i++) {
+            output += str;
+        }
+        return output;
+    }
 }
 
 module.exports = repeatString
