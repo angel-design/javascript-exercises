e06fd9ab5d72e45e7f6abc3703d33b72c554e344
Angel <cheangel@gmail.com>
2019-11-22 21:43:13 -0500

    Finish exercises

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,5 +1,5 @@
-function translate() {
-	// body...
+function translate(str) {
+	return str.replace(/(\b[aeiou]\w*)/g, "$1ay").replace(/(\b[^aeiou\s]*qu)(\w*)/g, "$2$1ay").replace(/(\b[^aeiqou\s]+(?=[aeiou]))(\w*)/g, "$2$1ay")
 }
 
 
--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,5 @@
-const snakeCase = function() {
-
+const snakeCase = function(str) {
+    return str.replace(/(\.{2,}[A-Z])/g, "_$1").replace(/(^(?!\w*_)\w*(?=[A-Z]))/g, "$1_").toLowerCase().replace(/\../g, "_").replace(/[\s\-]/g,"_").replace(/\W/g, "").replace(/\_{2,}/g, "_").replace(/(^\_|\_$)/g, "");
 }
 
 module.exports = snakeCase
